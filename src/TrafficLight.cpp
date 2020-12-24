db06a3de168c6f5fb7635fec3b8cf1372a149754
Lei Ge <gracegl2014@gmail.com>
2020-12-24 22:40:56 +0100

    update FP.3, not test

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -4,7 +4,6 @@
 
 /* Implementation of class "MessageQueue" */
 
-/* 
 template <typename T>
 T MessageQueue<T>::receive()
 {
@@ -19,7 +18,6 @@ void MessageQueue<T>::send(T &&msg)
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
 }
-*/
 
 /* Implementation of class "TrafficLight" */
 
@@ -54,5 +52,37 @@ void TrafficLight::cycleThroughPhases()
     // and toggles the current phase of the traffic light between red and green and sends an update method 
     // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
     // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
-    
+
+    std::random_device rd;
+    std::mt19937 eng(rd());
+    std::uniform_int_distribution<> distr(4, 6);
+    double cycleDuration = std::this_thread::sleep_for(std::chrono::seconds(distr(eng)));
+    std::chrono::time_point<std::chrono::system_clock> lastUpdate;
+
+    // init stop watch
+    lastUpdate = std::chrono::system_clock::now();
+    while(true)
+    {
+        // sleep at every iteration to reduce CPU usage
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+        // compute time difference to stop watch
+        long timeSinceLastUpdate = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - lastUpdate).count();
+        if (timeSinceLastUpdate >= cycleDuration * 1000)
+        {
+            if (_currentPhase ==  TrafficLightPhase::red)
+            {
+                _currentPhase = TrafficLightPhase::green;
+            }
+            else 
+            {
+                _currentPhase = TrafficLightPhase::red;
+            }
+            _messageQueue.send(std::move(_currentPhase));
+
+            // reset stop watch for next cycle
+            lastUpdate = std::chrono::system_clock::now();
+        }
+
+    }
 }
--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -25,8 +25,13 @@ template <class T>
 class MessageQueue
 {
 public:
+    T receive();
+    T send(T &&msg);
 
 private:
+    std::dequeue<T> _queue;
+    std::condition_variable _cond;
+    std::mutex _mutex;
     
 };
 
